c920b8bb25781f666f72ccd5849963fa355edbab
nikolai-kosolapov <kosolapov.nikolay90@ya.ru>
2018-09-19 00:14:02 -0700

    Added temp changes

--- Graphs/Algorithms/BFSVisitor.cs
+++ Graphs/Algorithms/BFSVisitor.cs
@@ -4,13 +4,35 @@
 
 namespace Graphs.Algorithms
 {
+    using System.Collections.Generic;
     using Graphs.Models;
 
     public abstract class BFSVisitor<T> : GraphVisitor<T>
     {
+        private Queue<Node<T>> queue = new Queue<Node<T>>();
+        private List<Node<T>> visited = new List<Node<T>>();
+
         public override void Visit(Node<T> node)
         {
-            
+            this.queue.Enqueue(node);
+            this.visited.Add(node);
+
+            while (this.queue.Count > 0)
+            {
+                var item = this.queue.Dequeue();
+                this.VisitNode(item);
+
+                foreach (var child in item.Adjacents)
+                {
+                    if (!this.visited.Contains(item))
+                    {
+                        this.queue.Enqueue(child);
+                        this.visited.Add(child);
+                    }
+                }
+
+                this.visited.Add(item);
+            }
         }
     }
 }
